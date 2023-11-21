32a6496f5eb287e4960b89ce1a9afecab430ac82
Diegolec18 <dlecanda18@gmail.com>
2023-11-21 10:33:21 -0600

    Agregando modulos de firebase

---
+++ docs/js/js firebase/modules/config-app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBYx4ZkabEr7lsbaNb2gT0K_Klg4WbLNKc",
+    authDomain: "mygameblog-89f58.firebaseapp.com",
+    projectId: "mygameblog-89f58",
+    storageBucket: "mygameblog-89f58.appspot.com",
+    messagingSenderId: "646506861033",
+    appId: "1:646506861033:web:af9e727081ac8827b97b78",
+    measurementId: "G-JE3CYKBH0C"
+  };
+
+
+export const app = initializeApp(firebaseConfig);
