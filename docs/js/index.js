71202b85ce1096096fdd78083b276e9189b0eccd
Grecia Lorena Morales Castillo <cdmx2510@amerike.edu.mx>
2023-11-28 06:49:06 -0600

    agregando imagenes, falta modificar

--- docs/js/index.js
+++ docs/js/index.js
@@ -14,3 +14,18 @@ function show()
     menuCategories.style.display = 'none'
     mainmenu.style.display = 'block'
 }
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function changeSlide(offset) {
+    showSlide(currentSlide + offset);
+}
+
+showSlide(currentSlide);
